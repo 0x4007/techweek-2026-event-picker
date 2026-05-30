905545100aeaa2c108a2876555f810287f7e29be
0x4007 <gpg@pavlovcik.com>
2026-05-30 17:19:08 -0400

    Use literal Deploy cron declaration

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,7 @@
-import { PARTIFUL_AUTO_SYNC_CRON_EXPRESSION, runPartifulAutoSync, startServer } from "./server.ts";
+import { runPartifulAutoSync, startServer } from "./server.ts";
 
-Deno.cron("partiful-auto-sync", PARTIFUL_AUTO_SYNC_CRON_EXPRESSION, () => {
-  void runPartifulAutoSync();
+Deno.cron("partiful-auto-sync", "* * * * *", async () => {
+  await runPartifulAutoSync();
 });
 
 startServer();
